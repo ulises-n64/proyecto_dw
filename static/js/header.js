8048461e0a1512a9e5164123877637d27d7fadf3
ulises-n64 <ulises.ra.cal@gmail.com>
2020-05-28 12:07:40 -0500

    agregue los archivos css

---
+++ static/js/header.js
@@ -0,0 +1,21 @@
+let $header = document.querySelector('#header');
+var $navegacion = document.getElementById('navegacion');
+
+window.addEventListener('scroll', function(e){
+	console.log(window.scrollY)
+	if(window.scrollY > 60){
+		$header.style.height = '48px';
+		$header.style.transition = 'all 0.2s';
+
+		$navegacion.style.opacity = '0';
+		$navegacion.style.transition = 'all 0.2s';
+
+	}else{
+		$header.style.height = '75px';
+		$header.style.transition = 'all 0.2s';
+		$navegacion.style.opacity = '1';
+		$navegacion.style.transition = 'all 0.2s';
+
+
+	}
+})
